7b076c470c6d2102f52d8d0d38bf5dd73848eef1
merukimon <mneiter@gmail.com>
2022-03-03 12:34:58 +0200

    create a user reducer

---
+++ src/store/index.ts
@@ -0,0 +1,4 @@
+import {createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+
+const store = createStore({}, applyMiddleware(thunk))
---
+++ src/store/reducers/userReducer.ts
@@ -0,0 +1,20 @@
+import { UserAction, UserAvtionType, UserState } from "../../types/todo";
+
+const initialUser: UserState = {
+    users: [],
+    loading: false,
+    error: null
+}
+
+export const userReduser = (state = initialUser, action: UserAction): UserState =>{
+    switch (action.type) {
+        case UserAvtionType.FETCH_USERS:
+            return {loading: true, error: null, users: []};
+        case UserAvtionType.FETCH_USERS_SUCCESS:
+            return {loading: false, error: null, users: action.payload};
+        case UserAvtionType.FETCH_USERS_ERROR:
+            return {loading: false, error: action.payload, users: []};
+        default:
+            return state;
+    }
+}
