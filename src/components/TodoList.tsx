2dfb3abd1954a37c94a5542de712594ed322efa7
merukimon <mneiter@gmail.com>
2022-03-06 08:50:12 +0200

    add pagination to component 'TodoList'

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,31 +3,42 @@ import { useAction } from '../hooks/useAction';
 import { useTypedSelector } from '../hooks/useTypedSelector';
 
 const TodoList: React.FC = () => {
-    const {todos, error, loading, page, limit} = useTypedSelector(state => state.todo)
-    const {fetchTodo, setTodoPage} = useAction()
+    const { todos, error, loading, page, limit } = useTypedSelector(state => state.todo)
+    const { fetchTodo, setTodoPage } = useAction()
+    const pages = [1, 2, 3, 4, 5]
 
-    useEffect(() =>{        
-        fetchTodo()
-    }, []);
+    useEffect(() => {
+        fetchTodo(page, limit)
+    }, [page]);
 
-    if(loading)
-    {
+    if (loading) {
         return <h1>data is loading ...</h1>
     }
     if (error) {
         return <h1>{error}</h1>
     }
-    
+
     return (
         <div>
-            {todos.map((todo) => (
-                    <div
-                        key={todo.id}>
-                       <h3>{todo.id} - {todo.title}</h3>
+            <div style={{ display: "flex" }}>
+                {pages.map((p) => (
+                    <div onClick={() => setTodoPage(p)}
+                        style={{ border: p === page ? '2px solid green' : '1px solid gray', padding: 10 }}
+                        key={p}>
+                        {p}
                     </div>
-                    ))}
+                ))}
+            </div>
+
+            {todos.map((todo) => (
+                <div
+                    key={todo.id}>
+                    <h3>{todo.id} - {todo.title}</h3>
+                </div>
+            ))}
         </div>
-  )
+
+    )
 }
 
 export default TodoList
